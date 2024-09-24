2ffd74b2cd1833930f8117c5f450045be8ad4f69
Gael Rosas Serratos <117534409+GaelRS@users.noreply.github.com>
2024-09-24 11:59:00 -0600

    feat:Se añadio paginación y agregación (#12)

--- RestApi/Repositories/GroupRepository.cs
+++ RestApi/Repositories/GroupRepository.cs
@@ -6,30 +6,36 @@ using RestApi.Models;
 
 namespace RestApi.Repositories;
 public class GroupRepository : IGroupRepository
-{
-    private readonly IMongoCollection<GroupEntity> _groups;
+{private readonly IMongoCollection<GroupEntity> _groups;
 
-    public GroupRepository(IMongoClient mongoClient, IConfiguration configuration)
-    {
-       var database = mongoClient.GetDatabase(configuration.GetValue<string>("MongoDb:Groups:DatabaseName"));
+    public GroupRepository(IMongoClient mongoClient, IConfiguration configuration) {
+        var database = mongoClient.GetDatabase(configuration.GetValue<string>("MongoDb:Groups:DatabaseName"));
         _groups = database.GetCollection<GroupEntity>(configuration.GetValue<string>("MongoDb:Groups:CollectionName"));
     }
+    
     public async Task<GroupModel> GetByIdAsync(string id, CancellationToken cancellationToken)
     {
-        try
-        {
+        try {
             var filter = Builders<GroupEntity>.Filter.Eq(x => x.Id, id);
             var group = await _groups.Find(filter).FirstOrDefaultAsync(cancellationToken);
+
             return group.ToModel();
-        }
-        catch(FormatException)
-        {
+        } catch(FormatException) {
             return null;
         }
     }
-    public async Task<IList<GroupModel>> GetByNameAsync(string name, CancellationToken cancellationToken){
+    public async Task<IList<GroupModel>> GetByNameAsync(string name, int page, int pageS, string orderBy, CancellationToken cancellationToken){
         var filter = Builders<GroupEntity>.Filter.Regex(group => group.Name, new BsonRegularExpression(name, "i"));
-        var groups = await _groups.Find(filter).ToListAsync(cancellationToken);
+        
+        var sort = Builders<GroupEntity>.Sort.Ascending(n => n.Name);
+
+        if (orderBy != "Name") {
+            sort = Builders<GroupEntity>.Sort.Descending(n => n.CreatedAt);
+        }
+
+        var groupsOr = _groups.Find(filter).Sort(sort).Skip((page - 1) * pageS).Limit(pageS);
+
+        var groups = await groupsOr.ToListAsync(cancellationToken);
         return groups.Select(group => group.ToModel()).ToList();
     }
 }
