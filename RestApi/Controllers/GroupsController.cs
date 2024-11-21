3be43b66aa83140f1f6089eef9fa0d5e655930b8
GaelRS <grosas04@alumnos.uaq.mx>
2024-11-21 00:09:05 -0600

    feat:Se añadio kubernetes

--- RestApi/Controllers/GroupsController.cs
+++ RestApi/Controllers/GroupsController.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RestApi.Dtos;
 using RestApi.Exceptions;
@@ -8,11 +9,12 @@ using RestApi.Services;
 namespace RestApi.Controllers;
 
 [ApiController]
+[Authorize]
 [Route("[controller]")]
 public class GroupsController : ControllerBase {
     private readonly IGroupService _groupService;
 
-    public GroupsController (IGroupService groupService) {
+    public GroupsController(IGroupService groupService) {
         _groupService = groupService;
     }
 
@@ -27,88 +29,84 @@ public class GroupsController : ControllerBase {
 
         return Ok(group.ToDto());
     }
-    
+
     [HttpGet]
-    public async Task<ActionResult<IList<GroupResponse>>> GetGroupByName([FromQuery] string name, 
-    [FromQuery] int page, [FromQuery] int pageS, [FromQuery] string orderBy, CancellationToken cancellationToken){
+    public async Task<ActionResult<IList<GroupResponse>>> GetGroupByName(
+        [FromQuery] string name, 
+        [FromQuery] int page, 
+        [FromQuery] int pageS, 
+        [FromQuery] string orderBy, 
+        CancellationToken cancellationToken) {
         
         var groups = await _groupService.GetGroupByNameAsync(name, page, pageS, orderBy, cancellationToken);
-
         return Ok(groups.Select(group => group.ToDto()).ToList());
-  
-
     }
-    
+
     [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteGroupById(string id, CancellationToken cancellationToken){
-      try{
-        await _groupService.DeleteGroupByIdAsync(id, cancellationToken);
-        return NoContent();
-      }catch(GroupNotFoundException){
-        return NotFound();
-      }
-        
+    public async Task<IActionResult> DeleteGroupById(string id, CancellationToken cancellationToken) {
+        try {
+            await _groupService.DeleteGroupByIdAsync(id, cancellationToken);
+            return NoContent();
+        } catch(GroupNotFoundException) {
+            return NotFound();
+        }
     }
 
     [HttpPost]
-    public async Task<ActionResult<GroupResponse>> CreatedGroup([FromBody] CreatedGroupRequest groupRequest, CancellationToken cancellationToken){
-        try{
-
+    public async Task<ActionResult<GroupResponse>> CreatedGroup(
+        [FromBody] CreatedGroupRequest groupRequest, 
+        CancellationToken cancellationToken) {
+        
+        try {
             var group = await _groupService.CreateGroupAsync(groupRequest.Name, groupRequest.Users, cancellationToken);
             return CreatedAtAction(nameof(GetGroupById), new {id = group.Id}, group.ToDto());
-
-        }catch(UserNotFoundException){
-            return NotFound(NewValidationProblemDetails("One or more validation errors occurred", HttpStatusCode.NotFound, new Dictionary<string, string[]>{
+        } catch(UserNotFoundException) {
+            return NotFound(NewValidationProblemDetails("One or more validation errors occurred", HttpStatusCode.NotFound, new Dictionary<string, string[]> {
                 {"Users",["User not found"]}
             }));
-        }
-        catch(InvalidGroupRequestFormatException){
-            
-            return BadRequest(NewValidationProblemDetails("One or more validation errors occurred", HttpStatusCode.BadRequest, new Dictionary<string, string[]>{
+        } catch(InvalidGroupRequestFormatException) {
+            return BadRequest(NewValidationProblemDetails("One or more validation errors occurred", HttpStatusCode.BadRequest, new Dictionary<string, string[]> {
                 {"Groups",["Users array is empty"]}
             }));
-
-        }catch(GroupAlreadyExistsException){
-            return Conflict(NewValidationProblemDetails("One or more validation errors occurred", HttpStatusCode.Conflict, new Dictionary<string, string[]>{
+        } catch(GroupAlreadyExistsException) {
+            return Conflict(NewValidationProblemDetails("One or more validation errors occurred", HttpStatusCode.Conflict, new Dictionary<string, string[]> {
                 {"Groups",["Group with same name already exists"]}
             }));
         }
     }
-
-
-    }
-
 
     [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateGroup (string id, [FromBody] UpdateGroupRequest groupRequest, CancellationToken cancellationToken){
-        try{
-
+    public async Task<IActionResult> UpdateGroup(
+        string id, 
+        [FromBody] UpdateGroupRequest groupRequest, 
+        CancellationToken cancellationToken) {
+        
+        try {
             await _groupService.UpdateGroupAsync(id, groupRequest.Name, groupRequest.Users, cancellationToken);
             return NoContent();
-
-        }catch(UserNotFoundException){
-            return NotFound(NewValidationProblemDetails("One or more validation errors occurred", HttpStatusCode.NotFound, new Dictionary<string, string[]>{
+        } catch(UserNotFoundException) {
+            return NotFound(NewValidationProblemDetails("One or more validation errors occurred", HttpStatusCode.NotFound, new Dictionary<string, string[]> {
                 {"Users",["User not found"]}
             }));
-        }
-        catch(GroupNotFoundException){
+        } catch(GroupNotFoundException) {
             return NotFound();
-        }
-        catch(InvalidGroupRequestFormatException){
-            
-            return BadRequest(NewValidationProblemDetails("One or more validation errors occurred", HttpStatusCode.BadRequest, new Dictionary<string, string[]>{
+        } catch(InvalidGroupRequestFormatException) {
+            return BadRequest(NewValidationProblemDetails("One or more validation errors occurred", HttpStatusCode.BadRequest, new Dictionary<string, string[]> {
                 {"Groups",["Users array is empty"]}
             }));
-
-        }catch(GroupAlreadyExistsException){
-            return Conflict(NewValidationProblemDetails("One or more validation errors occurred", HttpStatusCode.Conflict, new Dictionary<string, string[]>{
+        } catch(GroupAlreadyExistsException) {
+            return Conflict(NewValidationProblemDetails("One or more validation errors occurred", HttpStatusCode.Conflict, new Dictionary<string, string[]> {
                 {"Groups",["Group with same name already exists"]}
             }));
         }
     }
 
-    private static ValidationProblemDetails NewValidationProblemDetails(string title, HttpStatusCode statusCode, Dictionary<string, string[]>errors){
-        return new ValidationProblemDetails{
+    private static ValidationProblemDetails NewValidationProblemDetails(
+        string title, 
+        HttpStatusCode statusCode, 
+        Dictionary<string, string[]> errors) {
+        
+        return new ValidationProblemDetails {
             Title = title,
             Status = (int)statusCode,
             Errors = errors
--- RestApi/Program.cs
+++ RestApi/Program.cs
@@ -1,6 +1,10 @@
 using MongoDB.Driver;
 using RestApi.Repositories;
 using RestApi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +20,25 @@ builder.Services.AddSingleton<IMongoClient, MongoClient>(s => new MongoClient(bu
 builder.Services.AddScoped<IGroupService, GroupService>();
 builder.Services.AddScoped<IGroupRepository, GroupRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration.GetValue<string>("Authentication:Authority");
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
+            ValidateActor = false,
+            ValidateLifetime = true,
+            ValidateAudience = true,
+            ValidAudience = "groups-api",
+            ValidateIssuerSigningKey = true
+
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -25,6 +48,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 app.MapControllers();
